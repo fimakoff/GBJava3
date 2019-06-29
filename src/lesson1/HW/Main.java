ec238fb7d2dd0e63fa59a208fba73251a18a2011
fimakoff <fimakoff@gmail.com>
2019-06-30 01:03:59 +0300

    Добавил 1 и 2 задание

--- src/lesson1/HW/Main.java
+++ src/lesson1/HW/Main.java
@@ -1,4 +1,50 @@
 package lesson1.HW;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+        exercise1(2);
+        exercise2(5);
+    }
+
+    private static void exercise2(int nums) {
+        System.out.println("exercise 2");
+        Object[] arr = new Object[nums];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i+1;
+        }
+        System.out.println("Array: " + Arrays.toString(arr));
+        ArrayList<Object> arrayList = arrayToList(arr);
+        System.out.println(arrayList);
+    }
+
+    private static <T> ArrayList<T> arrayToList(T[] arr) {
+        ArrayList<T> arrayList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            arrayList.add((T) ("Element #" + i + ": " + arr[i]));
+        }
+        return arrayList;
+    }
+
+    private static void exercise1(int nums) {
+        System.out.println("exercise 1");
+        Object[] arr = new Object[nums];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= i;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(moveElements(arr)));
+    }
+
+    private static <T> T[] moveElements(T[] arr) {
+        T[] outputArray = (T[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            outputArray[i]=arr[arr.length-i-1];
+        }
+        return outputArray;
+    }
 }
