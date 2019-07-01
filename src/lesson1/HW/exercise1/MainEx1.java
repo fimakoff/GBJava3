d7d519b59174ca098acc2b0184ee1f35e5112729
fimakoff <fimakoff@gmail.com>
2019-07-01 15:45:05 +0300

    Рефактор задания №1. Скармливаем методу массив любого ссылочного типа.

--- src/lesson1/HW/exercise1/MainEx1.java
+++ src/lesson1/HW/exercise1/MainEx1.java
@@ -4,21 +4,32 @@ import java.util.Arrays;
 
 public class MainEx1 {
 
-    private static final int numsOfElement = 2;
+    private static final int numsOfElement = 5;
 
-    private static <T> T[] moveElements(T[] arr, int startIndex, int endIndex) {
+    public static void main(String[] args) {
+        String[] arrStr = new String[numsOfElement];
+        for (int i = 0; i < arrStr.length; i++) {
+            arrStr[i] = "El" + (i + 1);
+        }
+        printAndMoveElementsOfArray(arrStr, 1, 3);
+
+        Number[] arrNum = new Number[numsOfElement];
+        for (Integer i = 0; i < arrNum.length; i++) {
+            arrNum[i] = i;
+        }
+        printAndMoveElementsOfArray(arrNum, 0, 4);
+    }
+
+    private static <T> void printAndMoveElementsOfArray(T[] arr, int startIndex, int endIndex) {
+        System.out.println(arr.getClass().getComponentType());
+        System.out.println("Original array: " + Arrays.toString(arr));
+        moveElements(arr, startIndex, endIndex);
+        System.out.println("Modified array: " + Arrays.toString(arr) + "\n");
+    }
+
+    private static <T> void moveElements(T[] arr, int startIndex, int endIndex) {
         T temp = arr[endIndex];
         arr[endIndex] = arr[startIndex];
         arr[startIndex] = temp;
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        Object[] arr = new Object[numsOfElement];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = "El" + (i + 1);
-        }
-        System.out.println("Original array: " + Arrays.toString(arr));
-        System.out.println("Modified array: " + Arrays.toString(moveElements(arr,0,1)));
     }
 }
