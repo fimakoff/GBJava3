3335e1fcba4502ae42897031ad60519804a7afb8
fimakoff <fimakoff@gmail.com>
2019-07-02 10:51:02 +0300

    Изменение способа создания коробок. Теперь для создания коробки нужно только передать количество помещаемых  в фруктов в коробку. Добавлена возможность съесть фрукты из коробки. + дополнительные доработки

--- src/lesson1/HW/exercise3/MainEx3.java
+++ src/lesson1/HW/exercise3/MainEx3.java
@@ -1,3 +1,4 @@
+
 package lesson1.HW.exercise3;
 
 import lesson1.HW.exercise3.Fruits.Apple;
@@ -5,15 +6,16 @@ import lesson1.HW.exercise3.Fruits.Orange;
 
 class MainEx3 {
     private MainEx3() {
-        Box<Apple> appleBox1 = new Box<>();
-        Box<Apple> appleBox2 = new Box<>();
-        appleBox1.add(new Apple(), 5);
-        appleBox2.add(new Apple(), 5);
-
-        Box<Orange> orangeBox1 = new Box<>();
-        Box<Orange> orangeBox2 = new Box<>();
-        orangeBox1.add(new Orange(), 5);
-        orangeBox2.add(new Orange(), 6);
+        Apple ap = new Apple();
+        Orange or = new Orange();
+        Box<Apple> appleBox1 = new Box<>(ap);
+        Box<Apple> appleBox2 = new Box<>(ap);
+        appleBox1.add(5);
+        appleBox2.add(5);
+        Box<Orange> orangeBox1 = new Box<>(or);
+        Box<Orange> orangeBox2 = new Box<>(or);
+        orangeBox1.add(5);
+        orangeBox2.add(6);
 
         System.out.println("Вес коробки с яблоками №1: " + appleBox1.getWeightBox());
         System.out.println("Вес коробки с яблоками №2: " + appleBox2.getWeightBox());
@@ -21,7 +23,7 @@ class MainEx3 {
         System.out.println("\nВес коробки с апельсинами №1: " + orangeBox1.getWeightBox());
         System.out.println("Вес коробки с апельсинами №2: " + orangeBox2.getWeightBox());
         System.out.println("\nДобавим еще апельсин в коробку №2: ");
-        orangeBox2.add(new Orange());
+        orangeBox2.add();
         System.out.println("Вес коробки с апельсинами №2: " + orangeBox2.getWeightBox());
         System.out.println("Вес коробок с апельсинами одинаковый? " + orangeBox1.compareOfWeight(orangeBox2));
         System.out.println("\nВес коробок с разными фруктами одинаковый? " + appleBox1.compareOfWeight(orangeBox1));
@@ -29,7 +31,12 @@ class MainEx3 {
         appleBox1.intersperse(appleBox2);
         System.out.println("Вес коробки с яблоками №1: " + appleBox1.getWeightBox());
         System.out.println("Вес коробки с яблоками №2: " + appleBox2.getWeightBox());
-
+        System.out.println("Съели одно яблоко из коробки №1:");
+        appleBox1.eatFruit();
+        System.out.println("Вес коробки с яблоками №1 после поедания: " + appleBox1.getWeightBox());
+        System.out.println("Съели много яблок из коробки №2:");
+        appleBox2.eatFruits(4);
+        System.out.println("Вес коробки с яблоками №2 после поедания: " + appleBox2.getWeightBox());
     }
 
     public static void main(String[] args) {
