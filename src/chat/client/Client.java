5f9244cec713a08902a9eb43862ab9ae8eb939f7
fimakoff <fimakoff@gmail.com>
2019-07-10 11:24:20 +0300

    Константы в Client.java вынесены в отдельные поля. Обновление файла Readme и результата обработки команды '/help'

--- src/chat/client/Client.java
+++ src/chat/client/Client.java
@@ -9,14 +9,18 @@ import javafx.stage.Stage;
 
 public class Client extends Application {
 
+    private final int timeToLogin = 120;
+    private final int width = 600;
+    private final int height = 275;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
         primaryStage.getIcons().add(new Image("file:img/icon.png"));
         Controller ct = new Controller();
-        ct.timeOut(120);
+        ct.timeOut(timeToLogin);
         primaryStage.setTitle("Client");
-        primaryStage.setScene(new Scene(root, 500, 275));
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
     }
 
