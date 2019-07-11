17136ae18c5e05b9469f4549f9205be24a7612cb
fimakoff <fimakoff@gmail.com>
2019-07-11 13:44:04 +0300

    init commit

---
+++ src/lesson4/HW/WaitNotifyClass.java
@@ -0,0 +1,11 @@
+/*
+1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+2. На серверной стороне сетевого чата реализовать управление потоками через ExecutorService.
+*/
+package lesson4.HW;
+
+public class WaitNotifyClass {
+    public static void main(String[] args) {
+
+    }
+}
